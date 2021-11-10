f2ece38f7f82c051e7147e1ddeb6c1957611242e
Devin Jaggernauth <neexis@gmail.com>
2021-11-09 21:26:21 -0400

    create schemas

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(helmet());
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 const { PORT = 3000 } = process.env;
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 app.listen(PORT, () => {
-  console.log('Server is serving');
 });
 
 app.use('/cards', cardRouter);
