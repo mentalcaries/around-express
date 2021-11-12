8471dfd7d286cfe2d0edc3cbbec49252bbcb3543
Devin Jaggernauth <neexis@gmail.com>
2021-11-11 21:28:21 -0400

    start like card

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,26 +2,20 @@ const path = require('path');
 const getFileData = require('../utils/readFile');
 const Card = require('../models/card');
 
-const cardDataPath = path.join(__dirname, '../', 'data', 'cards.json');
-const ERROR_CODE = 400;
-
-// const getCards = (req, res) => getFileData(cardDataPath, res)
-//   .then((cards) => res.send(cards))
-//   .catch((err) => res.status(404).send(err));
-  
 const getCards = (req, res)=> {
   Card.find({})
   .orFail() // throws a DocumentNotFoundError
   .then((cardData) => {
     res.send(cardData); // skipped, because an error was thrown
   })
-  .catch((error) => {
-    // now this does run, so we can handle the error and return an appropriate message
+  .catch((err) => {
+    res.status(404).send(err)
   }); 
 }
 
 const createCard = (req, res) => {
-  console.log(req.user._id);
+  // console.log(req.user._id);
+  const ERROR_CODE = 400;
   const { name, link } = req.body;
   Card.create({ name, link })
     .then((card) => res.send({ data: card }))
@@ -33,7 +27,25 @@ const createCard = (req, res) => {
 };
 
 const deleteCard = (req, res) => {
-  console.log(req);
+  const ERROR_CODE = 404
+  Card.findByIdAndRemove({_id: req.params.cardId})
+  .orFail()
+  .then(cardData=>res.send({data: cardData}))
+  .catch(err=>{
+    if (err.name ==='DocumentNotFoundError')
+    return res.status(ERROR_CODE).send({message: 'No card with that ID found'})})
+
 };
 
-module.exports = { getCards, createCard, deleteCard };
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+  .orFail()
+  .then(cardData=>res.send({data: cardData}))
+  .catch(err=>res.status(404).send({message: 'garbage'}))
+}
+
+module.exports = { getCards, createCard, deleteCard, likeCard };
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const { getCards, createCard, deleteCard, likeCard } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCard);
+router.put('/:cardId/likes', likeCard);
 
 module.exports = router;
